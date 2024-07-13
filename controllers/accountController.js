bb3bbe182e0e50716fc1f20afad0a1b55cce8eb6
Boluwatife Adebiyi <omotoyinbobade15@gmail.com>
2024-07-14 00:40:19 +0100

    week-4team work

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,7 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
+
 /* ****************************************
  *  Deliver login view
  * *************************************** */
@@ -9,7 +11,9 @@ async function buildLogin(req, res, next) {
     res.render("account/login", {
       title: "Login",
       nav,
-      message
+      message,
+      errors: null,
+      account_email: ""
     });
   }
   
@@ -32,11 +36,25 @@ async function registerAccount(req, res, next) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult.rowCount) {
@@ -44,7 +62,7 @@ async function registerAccount(req, res, next) {
       "notice",
       `Congratulations, you\'re registered ${account_firstname}. Please log in.`
     )
-    res.status(201).redirect("account/login");
+    res.status(201).redirect("/account/login");
   } else {
     req.flash("notice", "Sorry, the registration failed.")
     res.status(501).render("account/register", {
@@ -57,40 +75,40 @@ async function registerAccount(req, res, next) {
 /* ****************************************
  *  Process Registration
  * *************************************** */
-async function registerAccount(req, res, next) {
-    let nav = await utilities.getNav();
-    const { account_firstname, account_lastname, account_email, account_password } = req.body;
+// async function registerAccount(req, res, next) {
+//     let nav = await utilities.getNav();
+//     const { account_firstname, account_lastname, account_email, account_password } = req.body;
 
-    try {
-        const regResult = await accountModel.registerAccount(
-            account_firstname,
-            account_lastname,
-            account_email,
-            account_password
-        );
+//     try {
+//         const regResult = await accountModel.registerAccount(
+//             account_firstname,
+//             account_lastname,
+//             account_email,
+//             account_password
+//         );
 
-        if (regResult.rowCount) {
-            req.flash(
-                "notice",
-                `Congratulations, you'\re registered ${account_firstname}. Please log in.`
-            );
-            res.status(201).redirect("/account/login"); 
-        } else {
-            req.flash("notice", "Sorry, the registration failed.");
-            res.status(501).render("account/register", {
-                title: "Registration",
-                nav,
-            });
-        }
-    } catch (error) {
-        console.error("Registration Error:", error);
-        req.flash("notice", "An unexpected error occurred. Please try again.");
-        res.status(500).render("account/register", {
-            title: "Registration",
-            nav,
-        });
-    }
-}
+//         if (regResult.rowCount) {
+//             req.flash(
+//                 "notice",
+//                 `Congratulations, you'\re registered ${account_firstname}. Please log in.`
+//             );
+//             res.status(201).redirect("/account/login"); 
+//         } else {
+//             req.flash("notice", "Sorry, the registration failed.");
+//             res.status(501).render("account/register", {
+//                 title: "Registration",
+//                 nav,
+//             });
+//         }
+//     } catch (error) {
+//         console.error("Registration Error:", error);
+//         req.flash("notice", "An unexpected error occurred. Please try again.");
+//         res.status(500).render("account/register", {
+//             title: "Registration",
+//             nav,
+//         });
+//     }
+// }
   
 module.exports = { buildLogin, buildRegister, registerAccount }
   
--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -1,5 +1,6 @@
 const utilities = require(".")
 const { body, validationResult } = require("express-validator")
+const accountModel = require("../models/account-model")
 
 const validate = {}
 
@@ -31,7 +32,13 @@ validate.registationRules = () => {
       .notEmpty()
       .isEmail()
       .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
+      .withMessage("A valid email is required.")
+      .custom(async (account_email) => {
+        const emailExists = await accountModel.checkExistingEmail(account_email)
+        if (emailExists){
+          throw new Error("Email exists. Please log in or use different email")
+        }
+      }),
   
       // password is required and must be strong password
       body("account_password")
@@ -69,6 +76,46 @@ validate.checkRegData = async (req, res, next) => {
     }
     next()
   }
+
+  /*  **********************************
+  *  Login Data Validation Rules
+  * ********************************* */
+validate.loginRules = () => {
+  return [
+    // valid email is required
+    body("account_email")
+      .trim()
+      .isEmail()
+      .normalizeEmail()
+      .withMessage("A valid email is required."),
+
+    // password is required
+    body("account_password")
+      .trim()
+      .notEmpty()
+      .withMessage("Password is required."),
+  ]
+}
+
+/* ******************************
+ * Check data and return errors or continue to login
+ * ***************************** */
+validate.checkLoginData = async (req, res, next) => {
+  const errors = validationResult(req)
+  const message = req.flash("notice");
+  if (!errors.isEmpty()) {
+    let nav = await utilities.getNav()
+    res.render("account/login", {
+      title: "Login",
+      nav,
+      message,  
+      errors,
+      account_email: req.body.account_email,
+    })
+    return
+  }
+  next()
+}
   
   module.exports = validate
   
